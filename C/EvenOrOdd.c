6edb7046a80c8a25d2e839c9af3e151f996d83c9
amanashu201 <70699624+amanashu201@users.noreply.github.com>
2020-10-25 16:44:57 +0530

    20201025

---
+++ C/EvenOrOdd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+	
+	printf("Please enter a number: ");
+	scanf("%d", &n);
+	
+	if(n%2==0)
+	{
+		printf("%d is even number.", n);
+	}
+	else
+	{
+		printf("%d is odd number.", n);
+	}
+	return 0;
+}
